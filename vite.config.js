1489dcaba78242887cba15b95fb114e6e72cbb94
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2026-02-08 20:52:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    chunkSizeWarningLimit: 1600
+    outDir: 'build', // Penting untuk Vercel
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    host: true // Agar bisa dibuka di HP saat mode dev (npm run dev)
   }
 })
